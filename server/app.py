5cd7caf46e3e5ce7f2b2eb58d95b4c1552844673
Parker Parham <parkerparham@gmail.com>
2023-05-22 17:18:53 -0400

    Adds routes for Items and Item

---
+++ server/app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+fakeDatabase = {
+    1:{'name':'Clean car'},
+    2:{'name':'Write blog'},
+    3:{'name':'Start stream'},
+}
+
+class Items(Resource):
+    def get(self):
+        return fakeDatabase
+    
+api.add_resource(Items, '/items')
+
+class Item(Resource):
+    def get(self, id):
+        item = fakeDatabase[id]
+        return item
+
+api.add_resource(Item, '/items/<int:id>')
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
